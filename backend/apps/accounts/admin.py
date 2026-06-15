b701bb212741e09c60a8e4cd523743c954e79556
hema-madhuri-mamidi <hemamadhuri0101@gmail.com>
2026-06-15 18:19:53 +0530

    merge backend and frontend

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Profile, WorkerProfile, ContractorProfile
+
+admin.site.register(Profile)
+admin.site.register(WorkerProfile)
+admin.site.register(ContractorProfile)
--- backend/niyogax/settings.py
+++ backend/niyogax/settings.py
@@ -13,11 +13,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
-load_dotenv()
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -28,6 +29,7 @@ SECRET_KEY = 'django-insecure-_jp)adv902%)mf8m9r2xawr(k1+@liirepxjbcxgnol6wwk%rt
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+USE_TEST_OTP = False
 
 ALLOWED_HOSTS = []
 
@@ -54,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -62,7 +65,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+# ]
 ROOT_URLCONF = 'niyogax.urls'
 
 TEMPLATES = [
@@ -112,7 +117,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+TWILIO_VERIFY_SERVICE_SID = os.getenv("TWILIO_VERIFY_SERVICE_SID")
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
@@ -136,4 +143,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # CORS Settings
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
